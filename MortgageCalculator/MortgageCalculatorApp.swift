9c5f3ae25c857f45c477c6c4b15f0bc5bbdff4ab
Sebastian Hajduk <shajduk0@gmail.com>
2022-05-24 19:06:07 +0200

    Initial Commit

---
+++ MortgageCalculator/MortgageCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MortgageCalculatorApp.swift
+//  MortgageCalculator
+//
+//  Created by Sebastian Hajduk on 24/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct MortgageCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
